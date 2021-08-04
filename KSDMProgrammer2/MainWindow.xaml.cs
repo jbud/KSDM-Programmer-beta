c4187b6249a3d825a8af3c50e6d46ec695b2895a
Joe Jackson <jbudorg@gmail.com>
2021-08-04 08:14:06 -0700

    1.9.9!
    Move all 1.0 functionality into 2.0.
    Fix Layout issues
    Introduce new color pallet

--- KSDMProgrammer2/MainWindow.xaml.cs
+++ KSDMProgrammer2/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,25 +16,99 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace KSDMProgrammer2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+
         private OpenFileDialog openFileDialog1 = new OpenFileDialog();
         private exe ex;
+        private string potential;
+        System.Windows.Threading.DispatcherTimer loadTimer;
         public MainWindow()
         {
+            loadTimer = new System.Windows.Threading.DispatcherTimer();
+            loadTimer.Tick += new EventHandler(loadTimer_tick);
+            loadTimer.Interval = new TimeSpan(0, 0, 1);
+            loadTimer.Start();
             openFileDialog1.Filter = "Binary files (*.hex)|*.hex|UF2 Files (*.uf2)|*.uf2";
             openFileDialog1.FileOk += new CancelEventHandler(openFileDialog1_FileOk);
             InitializeComponent();
+        }
+
+        private void loadTimer_tick(object sender, EventArgs e)
+        {
+            scan();
+            loadTimer.Stop();
+        }
+        private void scan()
+        {
+            Debug.WriteLine("Begin Scan");
             string[] nameArray;
-            nameArray = System.IO.Ports.SerialPort.GetPortNames();
+            nameArray = System.IO.Ports.SerialPort.GetPortNames();      // get a list of available ports
+            string typeFound = "";
+            bool found = false;
+            string tr;
+
+            // convoluted way to move COM1 to end of list, usually it's not what we're looking for but sometimes it can be.
+            tr = nameArray[0];
+            nameArray[0] = nameArray[nameArray.Length - 1];
+            nameArray[nameArray.Length - 1] = tr;
+
+            foreach (string b in nameArray)
+            {
+                string temp = exe.serialPoke(b);
+                if (temp.Contains("ksdm3"))
+                {
+                    found = true;
+                    if (temp.Contains("avr"))
+                    {
+                        typeFound = "KSDM3-avr";
+                        potential = b;
+                        break;
+                    }
+                    else if (temp.Contains("rp2040"))
+                    {
+                        typeFound = "KSDM3-rp2040";
+                        openFileDialog1.FilterIndex = 2;
+                        potential = b;
+                        break;
+                    }
+                }
+                continue;
+            }
+            if (found)
+                richTextBox1.Text = "Found " + typeFound + " at com port: " + potential + ".";
+            else
+                richTextBox1.Text = "KSDM could not be automatically found, manually select a port or contact support@stinger.store";
+
             comboBox1.ItemsSource = nameArray;
             comboBox1.SelectedIndex = 0;
+            int cindex = 0;
+
+            foreach (string prt in comboBox1.Items)
+            {
+                if (prt == potential || cindex == comboBox1.Items.Count)
+                {
+                    break;
+                }
+                cindex++;
+            }
+            if (comboBox1.Items.Count - 1 >= cindex)
+            {
+                comboBox1.SelectedIndex = cindex;
+            }
+        }
+        private void minimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowStyle = WindowStyle.SingleBorderWindow;
+            WindowState = WindowState.Minimized;
         }
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -53,24 +128,63 @@ namespace KSDMProgrammer2
         private void button_Click_1(object sender, RoutedEventArgs e)
         {
             openFileDialog1.ShowDialog();
-            
         }
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e) 
         {
             Debug.WriteLine("fileok");
+            flashBtn.IsEnabled = true;
             textBox1.Text = openFileDialog1.FileName;
+            richTextBox1.Foreground = Brushes.GreenYellow;
+            richTextBox1.Text = "Ready to flash...";
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            if (textBox1.Text.Contains(".uf2") || textBox1.Text.Contains(".hex"))
+            richTextBox1.Text = "Flashing device, please wait...";
+            Thread.Sleep(50);
+            ex = new exe(comboBox1.Text, textBox1.Text);
+            flashBtn.IsEnabled = false;
+            loopy();
+        }
+        private void loopy()
+        {
+            while (true)
             {
-                ex = new exe(comboBox1.Text, textBox1.Text);                // spawn AVRDUDE
+                if (!ex.success)                                        // TODO: Add fail detection based on AVRDUDE output.
+                {
+                    richTextBox1.Foreground = Brushes.Red;
+                    richTextBox1.Text = "Failed to flash KSDM3, contact support@stinger.store";
+
+                    return;
+                }
+                else if (ex.done)
+                {
+                    bool isRP = textBox1.Text.Contains("uf2");
+                    textBox1.Text = "";
+                    openFileDialog1.FileName = "";
+                    if (isRP)
+                        richTextBox1.Text = "Finished!";
+                    else
+                        richTextBox1.Text = ex.output;                      // show all output from AVRDUDE 
+                    richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                    richTextBox1.ScrollToEnd();
+
+                    return;
+                }
+                else
+                {
+                    continue;
+                }
             }
-            else
-            {
-                textBox1.Text = "Error, please select a hex file before continuing";
-            }
+        }
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ScanBtn_Click(object sender, RoutedEventArgs e)
+        {
+            scan();
         }
     }
 }
